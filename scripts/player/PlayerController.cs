0c08272b399ad323c9caee34a96e209e9337ba34
mind_blade <kanmear+github@gmail.com>
2023-12-19 19:57:47 +0300

    feat: jump window

--- scripts/player/PlayerController.cs
+++ scripts/player/PlayerController.cs
@@ -7,6 +7,9 @@ public partial class PlayerController : CharacterBody2D
 	[Export] private float speed = 300.0f;
 	[Export] private float jumpVelocity = -400.0f;
 	[Export] private float smoothDelta = 14f;
+	[Export] private float jumpWindow = 0.1f;
+	private float timeSinceLeftFloor;
+	private bool jumpUsed;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	private float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
@@ -14,11 +17,16 @@ public partial class PlayerController : CharacterBody2D
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
+		var isAbleToJump = IsAbleToJump((float)delta);
 
-		//TODO make a wrapper for IsOnFloor for a better jump
 		if (!IsOnFloor()) velocity.Y += gravity * (float)delta;
+		else jumpUsed = false;
 
-		if (Input.IsActionJustPressed("jump") && IsOnFloor()) velocity.Y = jumpVelocity;
+		if (!jumpUsed && Input.IsActionJustPressed("jump") && isAbleToJump)
+		{
+			velocity.Y = jumpVelocity;
+			jumpUsed = true;
+		}
 
 		bool leftInput = Input.IsActionPressed("left");
 		bool rightInput = Input.IsActionPressed("right");
@@ -30,5 +38,18 @@ public partial class PlayerController : CharacterBody2D
 
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+
+	private bool IsAbleToJump(float delta)
+	{
+		var isOnFloor = IsOnFloor();
+		if (!isOnFloor)
+		{
+			timeSinceLeftFloor += delta;
+			return timeSinceLeftFloor <= jumpWindow;
+		}
+
+		timeSinceLeftFloor = 0f;
+		return true;
 	}
 }
