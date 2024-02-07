790909c9cec4e8be21938cac2672882783f2f304
mind_blade <kanmear+github@gmail.com>
2024-02-07 19:50:28 +0300

    misc: hide cursor

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -5,4 +5,9 @@ namespace ProjectCleanSword.Scripts;
 public partial class Main : Node
 {
     public static Node2D Player { get; set; }
+
+    public override void _Ready()
+    {
+        Input.MouseMode = Input.MouseModeEnum.Hidden;
+    }
 }
