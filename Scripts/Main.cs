5f2d6ba4bca84148d0f22bfa543292e635f245f8
mind_blade <kanmear+github@gmail.com>
2024-03-02 20:17:32 +0300

    misc: grung spawn button

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -5,15 +5,28 @@ namespace ProjectCleanSword.Scripts;
 public partial class Main : Node
 {
     public static Node2D Player { get; set; }
+    [Export] private PackedScene enemy;
+    private RandomNumberGenerator generator;
 
     public override void _Ready()
     {
         Input.MouseMode = Input.MouseModeEnum.Hidden;
+        generator = new RandomNumberGenerator();
     }
 
     public override void _Process(double delta)
     {
         if (Input.IsActionJustPressed("pause")) 
             GetTree().Quit();
+
+        if (Input.IsActionJustPressed("spawn_grunt_DEBUG"))
+        {
+            Enemy.Enemy enemyInst = enemy.Instantiate() as Enemy.Enemy;
+            if (generator.RandiRange(0, 1) == 1)
+                enemyInst.Position = new Vector2(-200f, 504f);
+            else
+                enemyInst.Position = new Vector2(1300f, 504f);
+            GetParent().AddChild(enemyInst);
+        }
     }
 }
