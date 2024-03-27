922f220fc1465b1ed631097899c47c98d756b7e9
mind_blade <kanmear+github@gmail.com>
2024-03-27 12:57:44 +0300

    refactor: Attack OnBodyEntered method made static

--- Scripts/Player/PlayerAttack.cs
+++ Scripts/Player/PlayerAttack.cs
@@ -5,7 +5,7 @@ using Interfaces;
 
 public partial class PlayerAttack : Area2D
 {
-    private void OnBodyEntered(Node2D body)
+    private static void OnBodyEntered(Node2D body)
     {
         if (body is IDamageable damageable) 
             damageable.Damage(10);
